7d045c35338280a142139e74a1e46a62e28e7c59
Bruno Martins <bgcngm@gmail.com>
2020-09-24 11:51:33 +0100

    bacon: Use $(HOST_PREBUILT_TAG) where possible
    Change-Id: If211d23ce72b3404368cd723cbb9ea801e61802c

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -9,7 +9,7 @@
 
 FORTUNE_TARGET_PACKAGE := $(PRODUCT_OUT)/$(FORTUNE_VERSION).zip
 
-MD5 := prebuilts/build-tools/path/$(HOST_OS)-x86/md5sum
+MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
 
 .PHONY: fortune
 fortune: $(INTERNAL_OTA_PACKAGE_TARGET)
