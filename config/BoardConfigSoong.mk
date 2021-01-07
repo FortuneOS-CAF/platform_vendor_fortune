2d5a529d6b9fbd5e01b4db7951b52ad19bb1fbb3
StyloGey <stylogey@gmail.com>
2021-01-07 01:54:14 +0100

    interfaces: Introduce AOSPA power HAL for gestures.
    Change-Id: Icd7222888835d939da8032ace0fbdbb9fb555329

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -49,6 +49,38 @@ SOONG_CONFIG_fortuneGlobalVars_target_process_sdk_version_override := $(TARGET_P
 SOONG_CONFIG_fortuneGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
 SOONG_CONFIG_fortuneGlobalVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 
+# Gestures
+define add-gesturevar-if-exist
+$(eval vn := $(shell echo $(1) | tr '[:upper:]' '[:lower:]'))
+$(if $($(1)), \
+  $(eval SOONG_CONFIG_aospaGestureVars += $(vn)) \
+  $(eval SOONG_CONFIG_aospaGestureVars_$(vn) := $(patsubst "%",%,$($(1)))) \
+)
+endef
+
+SOONG_CONFIG_NAMESPACES += aospaGestureVars
+SOONG_CONFIG_aospaGestureVars :=
+GESTURE_SOONG_VARS := \
+    TARGET_GESTURES_NODE \
+    TARGET_TAP_TO_WAKE_NODE \
+    TARGET_TAP_TO_WAKE_EVENT_NODE \
+    TARGET_DRAW_V_NODE \
+    TARGET_DRAW_INVERSE_V_NODE \
+    TARGET_DRAW_O_NODE \
+    TARGET_DRAW_M_NODE \
+    TARGET_DRAW_W_NODE \
+    TARGET_DRAW_ARROW_LEFT_NODE \
+    TARGET_DRAW_ARROW_RIGHT_NODE \
+    TARGET_ONE_FINGER_SWIPE_UP_NODE \
+    TARGET_ONE_FINGER_SWIPE_RIGHT_NODE \
+    TARGET_ONE_FINGER_SWIPE_DOWN_NODE \
+    TARGET_ONE_FINGER_SWIPE_LEFT_NODE \
+    TARGET_TWO_FINGER_SWIPE_NODE \
+    TARGET_DRAW_S_NODE \
+    TARGET_SINGLE_TAP_TO_WAKE_NODE
+
+$(foreach v,$(GESTURE_SOONG_VARS),$(eval $(call add-gesturevar-if-exist,$(v))))
+
 # Qualcomm variables
 SOONG_CONFIG_NAMESPACES += aosp_vs_qva
 SOONG_CONFIG_aosp_vs_qva += aosp_or_qva
