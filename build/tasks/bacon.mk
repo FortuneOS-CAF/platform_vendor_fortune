2eee0b81feb5b8deb107d7c99dece40632632fab
Julian Veit <Claymore1298@gmail.com>
2021-05-01 11:56:15 +0200

    bacon: switch to sha256 checksum
    As md5 is not considered as secure since some time, and sha256 is
    industry standard, lets switch over here too.
    
    Change-Id: I22015be20126a4348affa5ef97e2c28fbd7b4717

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -9,10 +9,10 @@
 
 FORTUNE_TARGET_PACKAGE := $(PRODUCT_OUT)/$(FORTUNE_VERSION).zip
 
-MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
+SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
 .PHONY: fortune
 fortune: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(FORTUNE_TARGET_PACKAGE)
-	$(hide) $(MD5) $(FORTUNE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(FORTUNE_TARGET_PACKAGE).md5sum
+	$(hide) $(SHA256) $(FORTUNE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(FORTUNE_TARGET_PACKAGE).sha256sum
 	@echo "Package Complete: $(FORTUNE_TARGET_PACKAGE)" >&2
