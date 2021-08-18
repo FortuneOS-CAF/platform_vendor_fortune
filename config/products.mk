2bb592d374a65e27288a6a2767927d1a904c0732
Vishalcj17 <vishalcj17@gmail.com>
2021-08-18 23:32:11 +0530

    fortune: config: Include Snapdragon Clang makefile
    Change-Id: Ibb54342994005cdd43fc0f185e505573b6e3b6a3

---
+++ config/products.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2024 FortuneOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Include definitions for Snapdragon Clang
+$(call inherit-product, vendor/qcom/sdclang/config/SnapdragonClang.mk)
