811d62f23b4b7ea0909df91d8234b4eb468e4dd2
Vishalcj17 <vishalcj17@gmail.com>
2021-08-19 11:30:42 +0530

    fortune: config: Build QTI Framework detection
    Change-Id: I7d415d2eb38419cce3e3d81318133d662c1fe763

--- config/packages.mk
+++ config/packages.mk
@@ -10,3 +10,10 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0.vendor
+
+# QTI VNDK Framework Detect
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti \
+    libqti_vndfwk_detect \
+    libvndfwk_detect_jni.qti.vendor \
+    libqti_vndfwk_detect.vendor
