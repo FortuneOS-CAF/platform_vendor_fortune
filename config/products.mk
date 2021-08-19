0c762358a995a7e172681a38dfea55f0b61c3cb7
Vishalcj17 <vishalcj17@gmail.com>
2021-08-19 11:34:39 +0530

    fortune: Bring-in QTI Privapp and whitelist permissions
    Change-Id: If8fd3adda8fd062f215d05873dd6cf21f74f88a5

--- config/products.mk
+++ config/products.mk
@@ -16,3 +16,8 @@ $(call inherit-product, vendor/fortune/config/properties.mk)
 
 # Move Wi-Fi modules to vendor
 PRODUCT_VENDOR_MOVE_ENABLED := true
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/fortune/config/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    vendor/fortune/config/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
