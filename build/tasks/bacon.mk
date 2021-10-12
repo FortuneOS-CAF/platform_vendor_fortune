5bc9b242b4bab6a552e8a380f1206f133e21fb4d
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-10-12 19:09:29 +0530

    vendor: Add build finished info
    Change-Id: I6b7a45705bb20280ed71a8226fa0d57530c28747
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    
    -----
    
    vendor: Change MD5 to SHA256
    
    * Now we generate SHA256 instead of MD5
    
    Change-Id: I464fd2540005599681e8a896d95f35721f61145e
    Co-authored-by: xAbdaziz <i@abdaziz.dev>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -11,8 +11,18 @@ FORTUNE_TARGET_PACKAGE := $(PRODUCT_OUT)/$(FORTUNE_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
+CL_PRP="\033[35m"
+CL_RED="\033[31m"
+CL_GRN="\033[32m"
+
 .PHONY: fortune
 fortune: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(FORTUNE_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(FORTUNE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(FORTUNE_TARGET_PACKAGE).sha256sum
-	@echo "Package Complete: $(FORTUNE_TARGET_PACKAGE)" >&2
+	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
+	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(FORTUNE_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(FORTUNE_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(FORTUNE_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Integer Value:"${CL_RED}" `wc -c $(FORTUNE_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
