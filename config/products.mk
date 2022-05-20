bd8111c80af30a5ee6f4bc8b85dc16e4d7305ac5
Jiakai Zhang <jiakaiz@google.com>
2022-05-20 14:58:28 +0100

    fortune: Preopt SystemUI
    Bug: 232860596
    Test: See no dex2oat invocation on the first boot.
    Change-Id: I051c3b4f7c1c695f487d527b652203e6e007aa8d

--- config/products.mk
+++ config/products.mk
@@ -59,6 +59,10 @@ SKIP_BOOT_JARS_CHECK := true
 # leave less information available via JDWP.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
+# Pre-optimization
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Sensitive phone numbers and APN configurations
 PRODUCT_COPY_FILES += \
     vendor/fortune/prebuilt/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml \
