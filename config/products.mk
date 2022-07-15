4fa096dd970cc2afd9b70338bc195e462118339c
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-07-15 03:59:18 +0000

    fortune: Move Qualcomm specific configs to qcom/common
    [1] - Now we have Pixel 6 here, which doesn't use any Qualcomm,
          so let's move it to the appropriate place.
    
    Change-Id: I534ce11753b5cb7a7b892c5e4c441951b312369b

--- config/products.mk
+++ config/products.mk
@@ -48,9 +48,7 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    vendor/fortune/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
-    vendor/fortune/config/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
-    vendor/fortune/config/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+    vendor/fortune/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # Skip boot JAR checks
 SKIP_BOOT_JARS_CHECK := true
