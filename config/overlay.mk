36a936f1e816a6529ab923e2f84e5059658c2ad4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-02 20:32:50 +0530

    overlay: Define partition order for RROs
    Give vendor and odm higher priority over product as we ship common
    overlays in product and device-specific ones in vendor/odm. They're
    also placed higher than oem as we don't build the oem partition.
    System_ext is placed over vendor in order to override SoC-specific
    overlays via common.
    
    Change-Id: Ie8b6e79dcaf1fa1e896ee836a56e112f26023f01

--- config/overlay.mk
+++ config/overlay.mk
@@ -15,3 +15,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/fortune/overlay/static
 PRODUCT_PACKAGE_OVERLAYS += vendor/fortune/overlay/static
+
+PRODUCT_COPY_FILES += \
+    vendor/fortune/overlay/partition_order.xml:$(TARGET_COPY_OUT_PRODUCT)/overlay/partition_order.xml
