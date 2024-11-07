e8595e278b5938fd345206c5e06bf3870643dc00
Kizziama <kizziama@proton.me>
2024-11-07 03:56:57 +0000

    fortune: Move ThemedIcon to extras
    Change-Id: I9fc4b1e6076f3772b6d8a12c3a710b465d3eb5a3

--- config/overlay.mk
+++ config/overlay.mk
@@ -13,8 +13,5 @@ PRODUCT_PACKAGES += \
     FortuneSettingsProviderOverlay \
     FortuneSystemUIOverlay
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/fortune/overlay/static
-PRODUCT_PACKAGE_OVERLAYS += vendor/fortune/overlay/static
-
 PRODUCT_COPY_FILES += \
     vendor/fortune/overlay/partition_order.xml:$(TARGET_COPY_OUT_PRODUCT)/overlay/partition_order.xml
