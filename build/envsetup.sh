RESISTERR/vendor_lighthouse

build/envsetup: fix tag handling when calling merge-tag from env func

build/envsetup.sh
@@ -8,7 +8,7 @@ EOF
 function clomerge()
 {
     T=$(gettop)
-    bash $T/vendor/lighthouse/build/tools/merge-tag.sh
+    bash $T/vendor/lighthouse/build/tools/merge-tag.sh $1
 }
 
 export SKIP_ABI_CHECKS="true"

FortuneOS-CAF/platform_vendor_fortune

Change-Id: Id718302d1ed423ca1b18e19030cf53909b6e9b27

build/envsetup.sh
@@ -8,7 +8,7 @@ EOF
 function clomerge()
 {
     T=$(gettop)
-    bash $T/vendor/fortune/build/tools/merge-tag.sh
+    bash $T/vendor/fortune/build/tools/merge-tag.sh $1
 }
 
 export SKIP_ABI_CHECKS="true"
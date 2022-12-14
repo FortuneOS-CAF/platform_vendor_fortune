fcca1f7e04b0cb325388b06e2cfee507095414b3
Stealth1226 <rishawniyer06@gmail.com>
2022-12-14 23:06:49 +0530

    build/envsetup: fix tag handling when calling merge-tag from env func
    Change-Id: Id718302d1ed423ca1b18e19030cf53909b6e9b27

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -8,7 +8,7 @@ EOF
 function clomerge()
 {
     T=$(gettop)
-    bash $T/vendor/fortune/build/tools/merge-tag.sh
+    bash $T/vendor/fortune/build/tools/merge-tag.sh $1
 }
 
 export SKIP_ABI_CHECKS="true"
