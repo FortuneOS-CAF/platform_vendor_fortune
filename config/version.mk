cb32b749ab18643bacd03e5607946504f78510d2
Kizziama <kizziama@proton.me>
2025-03-09 14:47:08 +0000

    fortune: Refine versioning
    Change-Id: I656007a4d01036725529a6f2d99a896b48cc9558

--- config/version.mk
+++ config/version.mk
@@ -7,26 +7,18 @@
 # CLO Revision
 CLO_REVISION := LA.QSSI.15.0.r1-14100-qssi.0
 
-FORTUNE_BRAND := FortuneOS
-FORTUNE_BASE := CLO
-FORTUNE_BUILD_VERSION := 15.0
+FORTUNE_BUILD_VERSION := 1.0
+FORTUNE_BUILD_DATE := $(shell date +"%d%m%Y-%H%M")
+FORTUNE_CODENAME := Viola
 FORTUNE_RELEASETYPE ?= release
 
-FORTUNE_DATE_YEAR := $(shell date -u +%Y)
-FORTUNE_DATE_MONTH := $(shell date -u +%m)
-FORTUNE_DATE_DAY := $(shell date -u +%d)
-FORTUNE_DATE_HOUR := $(shell date -u +%H)
-FORTUNE_DATE_MINUTE := $(shell date -u +%M)
-FORTUNE_BUILD_DATE_UTC := $(shell date -d '$(FORTUNE_DATE_YEAR)-$(FORTUNE_DATE_MONTH)-$(FORTUNE_DATE_DAY) $(FORTUNE_DATE_HOUR):$(FORTUNE_DATE_MINUTE) UTC' +%s)
-FORTUNE_BUILD_DATE := $(FORTUNE_DATE_YEAR)$(FORTUNE_DATE_MONTH)$(FORTUNE_DATE_DAY)-$(FORTUNE_DATE_HOUR)$(FORTUNE_DATE_MINUTE)
-
-FORTUNE_VERSION := $(FORTUNE_BRAND)-$(FORTUNE_BASE)_$(FORTUNE_BUILD)-$(FORTUNE_BUILD_VERSION)-$(FORTUNE_BUILD_DATE)
+FORTUNE_VERSION := \
+    FortuneOS-v$(FORTUNE_BUILD_VERSION)-$(FORTUNE_BUILD)-$(FORTUNE_BUILD_DATE)-$(FORTUNE_RELEASETYPE)
 
 PRODUCT_SYSTEM_PROPERTIES += \
-    org.fortune.brand=$(FORTUNE_BRAND) \
     org.fortune.build.version=$(FORTUNE_BUILD_VERSION) \
     org.fortune.build_date=$(FORTUNE_BUILD_DATE) \
-    org.fortune.build_date_utc=$(FORTUNE_BUILD_DATE_UTC) \
+    org.fortune.codename=$(FORTUNE_CODENAME) \
     org.fortune.clo.revision=$(CLO_REVISION) \
     org.fortune.device=$(FORTUNE_BUILD) \
     org.fortune.releasetype=$(FORTUNE_RELEASETYPE) \
