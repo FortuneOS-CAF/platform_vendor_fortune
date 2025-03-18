333bbcfdb6ecca1e98ad655c8b05f35c68e2b036
Kizziama <kizziama@proton.me>
2025-03-18 06:15:44 +0000

    vendor: Build ThemePicker
    Change-Id: I5ad8215cc99d1ad169f51655b24baf0b0da32055

--- config/packages.mk
+++ config/packages.mk
@@ -119,6 +119,10 @@ PRODUCT_PACKAGES += \
     libtextclassifier_actions_suggestions_universal_model \
     libtextclassifier_lang_id_model
 
+# Theme Picker
+PRODUCT_PACKAGES += \
+    ThemePicker
+
 # WiFi
 PRODUCT_PACKAGES += \
     libwpa_client
